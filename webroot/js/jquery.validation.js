2a7eef124707bae5e3e4a5c887975cb66882324e
An M <anmichel@gmail.com>
2010-03-08 12:03:38 -0430

    Actualizando a cake 1.3

--- webroot/js/jquery.validation.js
+++ webroot/js/jquery.validation.js
@@ -13,16 +13,16 @@
 (function($) {
   var options = null;
   var errors = [];
-  
+
   $.fn.validate = function(rules, opts) {
     options = $.extend({}, $.fn.validate.defaults, opts);
-		
+
 		$.each(opts.watch, function(fieldId) {
 			$("#" + opts.watch[fieldId]).change(function() {
 				$.fn.validate.ajaxField($(this));
 			});
 		});
-		
+
 		$.each(rules, function(field) {
 		  var input = $("#" + field);
 		  var validationRules = this;
@@ -38,7 +38,7 @@
 
     return this.each(function() {
       $this = $(this);
-      $this.submit(function() {   
+      $this.submit(function() {
         $.fn.validate.beforeFilter();
         errors = [];
         $.each(rules, function(field) {
@@ -47,18 +47,18 @@
             return result;
           }
         });
-      
+
         $.fn.validate.afterFilter(errors);
 
         if(errors.length > 0) {
           return false;
         }
-        
+
         return true;
       });
     });
   };
-  
+
   $.fn.validate.validateField = function (field,validationRules) {
     var val = $("#" + field).val();
     var fieldName = $('#' + field).attr('name');
@@ -69,7 +69,7 @@
       if ($("#" + field).attr("id") == undefined) {
         return true;
       }
-      
+
       if (this['allowEmpty'] && typeof val == "string" && val == '') {
         return true;
       }
@@ -77,23 +77,23 @@
       if (this['allowEmpty'] && typeof val == "object" && val == null) {
         return true;
       }
-      
+
       if (!$.fn.validate.validateRule(val, this['rule'], this['negate'], fieldName)) {
         errors.push(this['message']);
         $.fn.validate.setError(field, this['message']);
-        
+
         if (this['last'] === true) {
           return false;
         }
       }
     });
   }
-  
+
   $.fn.validate.validateRule = function(val, rule, negate, fieldName) {
     if(negate == undefined) {
       negate = false;
     }
-        
+
     //handle custom functions
     if(typeof rule == 'object') {
       if($.fn.validate[rule.rule] != undefined) {
@@ -109,41 +109,41 @@
     } else if (!negate && !val.match(eval(rule))) {
       return false;
     }
-    
+
     return true;
   };
-  
-  $.fn.validate.boolean = function(val) {
+
+  $.fn.validate.bool = function(val) {
     return $.fn.validate.inList(val, [0, 1, '0', '1', true, false]);
   };
-    
+
   $.fn.validate.comparison = function(val, params) {
     if(val == "") {
       return false;
     }
-    
+
     val = Number(val);
     if(val == "NaN") {
       return false;
     }
-    
+
     if(eval(val + params[0] + params[1])) {
       return true;
     }
-    
+
     return false;
   };
-  
+
   $.fn.validate.inList = function(val, params) {
     if(params != null) {
       if($.inArray(val, params) == -1) {
         return false;
       }
     }
-    
+
     return true;
   };
-  
+
   $.fn.validate.range = function(val, params) {
     if (val < parseInt(params[0])) {
       return false;
@@ -151,22 +151,22 @@
     if (val > parseInt(params[1])) {
       return false;
     }
-    
+
     return true;
   };
-  
+
   $.fn.validate.multiple = function(val, params) {
     if(typeof val != "object" || val == null) {
       return false;
     }
-    
+
     if(params.min != null && val.length < params.min) {
       return false;
     }
     if(params.max != null && val.length > params.max) {
       return false;
     }
-    
+
     if(params["in"] != null) {
       for(i = 0; i < params["in"].length; i ++) {
         if($.inArray(params["in"][i], val) == -1) {
@@ -174,10 +174,10 @@
         }
       }
     }
-    
+
     return true;
   };
-	
+
 	$.fn.validate.ajaxField = function($field) {
 		$.fn.validate.clearError($field);
 		$.fn.validate.ajaxBeforeFilter($field);
@@ -193,7 +193,7 @@
 			},
 			"json");
 	}
-	
+
 	$.fn.validate.ajaxBeforeFilter = function($field) {
 		$field.after("<img class=\"ajax-loader\" src=\"" + options.root + "js_validate/img/ajax-loader.gif\">");
 	}
@@ -201,33 +201,33 @@
 	$.fn.validate.ajaxAfterFilter = function($field) {
 		$field.siblings(".ajax-loader").remove();
 	}
-	
+
 	$.fn.validate.clearError = function($field) {
 		if(typeof $field == "string") {
 			$field = $("#" + field);
 		}
-		
+
 		$field.removeClass("form-error")
 					.parents("div:first").removeClass("error")
-					.children(".error-message").remove();		
+					.children(".error-message").remove();
 	}
-  
+
   $.fn.validate.setError = function(field, message) {
     $("#" + field).addClass("form-error")
                   .parents("div:first").addClass("error")
                   .append('<div class="error-message">'  + message +  '</div>');
   };
-	
+
   $.fn.validate.beforeFilter = function() {
     if(options.messageId != null) {
       $("#" + options.messageId).html("")
                                 .slideDown();
     }
-    
+
     $(".error-message").remove();
     $("input").removeClass("form-error");
     $("div").removeClass("error")
-  };  
+  };
 
   $.fn.validate.afterFilter = function(errors) {
     if(options.messageId != null) {
